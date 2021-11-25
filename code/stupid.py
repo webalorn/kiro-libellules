73f26ecd0d46b98a9ff8379b86f96a640983e5e6
dido11 <v_m_1732@orange.fr>
2021-11-25 18:31:25 +0100

    randomized merge

--- code/stupid.py
+++ code/stupid.py
@@ -97,10 +97,13 @@ def merge(in_data, clients, sites, parents):
                 var_sites_demand[a] += var_sites_demand[b]
                 var_sites_demand[b] = 0
 
+                if var_sites_demand[a] > thresh:
+                    var_sites[a] = t_auto
+
                 val = util.eval_sol(in_data, gen_sol(var_clients, var_sites, var_parents))
 
-                print(val, best)
                 if val < best:
+                    print('#', end="")
                     aBest = a
                     bBest = b
                     best = val
@@ -109,6 +112,8 @@ def merge(in_data, clients, sites, parents):
                     best_sites = var_sites
                     best_parents = var_parents
                     best_sites_demand = var_sites_demand
+                else:
+                    print('.', end="")
 
         if aBest!=bBest:
             clients = best_clients
@@ -118,7 +123,81 @@ def merge(in_data, clients, sites, parents):
             
             print("ah!")
 
+        print("")
+
     return gen_sol(clients, sites, parents)
+
+def merge2(in_data, clients, sites, parents):
+    nbSites = len(in_data["sites"])
+    nbClients = len(in_data["clients"])
+
+    sites_demand = [0 for _ in range(nbSites)]
+
+    demand = [c[0] for c in in_data["clients"]]
+    
+    for iC in range(nbClients):
+        src = clients[iC]
+        if sites[src] == t_distrib:
+            src = parents[src]
+        sites_demand[src] += demand[iC]
+
+    aBest = 0
+    bBest = 1
+
+    while aBest!=bBest:
+        aBest = 0
+        bBest = 0
+
+        best = util.eval_sol(in_data, gen_sol(clients, sites, parents))
+        best_clients = clients
+        best_sites = sites
+        best_parents = parents
+
+        for a in range(nbSites):
+            for b in range(nbSites):
+                if a==b: continue
+                if sites[a] not in [t_prod, t_auto] or sites[b] not in [t_prod, t_auto]: continue
+                if sites_demand[a]+sites_demand[b] > max_cap: continue
+                
+                var_clients = [a if k==b else k for k in clients]
+                var_sites = sites.copy()
+                var_sites[b] = t_distrib
+                var_parents = parents.copy()
+                var_parents[b] = a
+                var_sites_demand = sites_demand.copy()
+                var_sites_demand[a] += var_sites_demand[b]
+                var_sites_demand[b] = 0
+
+                if var_sites_demand[a] > thresh:
+                    var_sites[a] = t_auto
+
+                val = util.eval_sol(in_data, gen_sol(var_clients, var_sites, var_parents))*(1+0.01*(0.5-random.random()))
+
+                if val < best:
+                    print('#', end="")
+                    aBest = a
+                    bBest = b
+                    best = val
+
+                    best_clients = var_clients
+                    best_sites = var_sites
+                    best_parents = var_parents
+                    best_sites_demand = var_sites_demand
+                else:
+                    print('.', end="")
+
+        if aBest!=bBest:
+            clients = best_clients
+            sites = best_sites
+            parents = best_parents
+            sites_demand = best_sites_demand
+            
+            print("ah!")
+
+        print("")
+
+    return gen_sol(clients, sites, parents)
+
 
 
 def stupid(in_data):
@@ -131,6 +210,15 @@ def stupid(in_data):
     return gen_sol(cli, sit, par)
 
 def stupid2(in_data):
+    nbSites = len(in_data["sites"])
+
+    cli = clients_setup(in_data)
+    sit = gen_prod(in_data, cli)
+    par = [0 for _ in range(nbSites)]
+
+    return merge(in_data, cli, sit, par)
+
+def stupid3(in_data):
     nbSites = len(in_data["sites"])
 
     cli = clients_setup(in_data)
