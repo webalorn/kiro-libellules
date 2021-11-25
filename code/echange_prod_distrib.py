6174ad75e1f8acae316bd03980ac16269824cbc7
moquem <maena.quemener@yahoo.com>
2021-11-25 19:55:42 +0100

    urgence

--- code/echange_prod_distrib.py
+++ code/echange_prod_distrib.py
@@ -20,9 +20,12 @@ def echange_try(in_data,sol):
     parent = sol['parent']
     sites = sol['sites']
 
+    print(len(parent),len(sites))
+
     n = random.randint(0,len(parent))
     while sites[n] != 1 or sites[n] != 2:
         n = random.randint(0,len(parent))
+        print(n)
     
     fils = recup_fils(parent,n)
     indice_f = random.randint(0,len(fils))
