9235256119bbd467a1c5ff4d03a5bb33af4d432d
dido11 <v_m_1732@orange.fr>
2021-11-25 14:29:05 +0100

    + Added constants

--- code/util.py
+++ code/util.py
@@ -15,6 +15,24 @@ INPUT_NAMES = [e.name for e in Path('../inputs').iterdir() if e.name.endswith('.
 
 OUT_SUFFIX = '-out-1' # TODO : to have different solutions names
 
+# ========== Constants ==========
+
+cost_build_prod = 800000.0
+cost_automation = 1200000.0
+cost_build_distrib = 60000.0
+
+cost_prod_prod = 18.0
+cost_prod_auto_bonus = 3.4
+cost_prod_distrib = 2.0
+
+cost_route_primary = 0.015
+cost_route_secondary = 3.63
+
+cost_capacity_exceed = 1000.0
+capacity_base = 750000
+capacity_auto_bonus = 225000
+
+
 # ========== Input / Output ==========
 
 def preprocess_input(data):
@@ -70,11 +88,17 @@ def output_sol_if_better(name, data):
 
 # ========== Evaluation ==========
 
+def total_cost(data):
+    ret = 0
+    # Building cost
+    for s in data["productionCenters"]:
+        pass
+
 def eval_sol(data):
     return 0
 
 def is_better_sol(old_sol_value, new_sol_value):
-    return new_sol_value > old_sol_value # TODO : Replace by < if the best value is the lower one
+    return new_sol_value < old_sol_value # TODO : Replace by < if the best value is the lower one
             
 
 # ========== Utilities ==========
